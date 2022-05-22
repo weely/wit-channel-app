9e6dd1eff3fcdd62fd66162d9b6b63194e3d4357
Weely .cc <lm220930@163.com>
2022-05-22 14:09:34 +0800

    feat: add palce order

--- pages/order/place/index.js
+++ pages/order/place/index.js
@@ -1,6 +1,10 @@
-import { config } from '../../../config/index'
-import { checkLocationAuth } from '../../../utils/location'
-import { getBase64Url } from '../../../utils/util'
+import {
+  config
+} from '../../../config/index'
+import {
+  checkLocationAuth
+} from '../../../utils/location'
+import { getDistance } from '../../../utils/util'
 const app = getApp()
 const QQMapWX = require('../../../libs/qqmap-wx-jssdk.js')
 let qqmapsdk;
@@ -9,47 +13,90 @@ Page({
   data: {
     hasLocationAuth: null,
     goods: {},
-    address: '',
-    svgPosition: ''
+    adInfo: {
+      name: '获取定位中...',
+      address: '',
+      latitude: '',
+      longitude: ''
+    },
   },
   async init() {
-    this.getPositionSvg()
-    const hasLocationAuth =  await checkLocationAuth(this)
-    this.setData({ hasLocationAuth })
-    qqmapsdk =  new QQMapWX({ key: config.mapKey })
+    const hasLocationAuth = await checkLocationAuth(this)
+    this.setData({
+      hasLocationAuth
+    })
+    qqmapsdk = new QQMapWX({
+      key: config.mapKey
+    })
     const eventChannel = this.getOpenerEventChannel()
-    eventChannel && eventChannel.on('acceptDataFromOpenerPage', (goods) => {
-      this.setData({ goods })
+    eventChannel && eventChannel.on && eventChannel.on('acceptDataFromOpenerPage', (goods) => {
+      this.setData({
+        goods
+      })
     })
     this.getLocation()
   },
-  getPositionSvg() {
-    // console.log(app.require('/assets/icons/position'))
-    wx.getFileSystemManager().readFile({
-      filePath: '../../../assets/icons/position.svg',
-      success: res=> {
-        console.log(res)
-        const svgPosition = getBase64Url(res)
-        console.log(svgPosition)
-      },
-      fail: err => {
-        console.log(err)
+  getLocation() {
+    const context = this
+    wx.getLocation({
+      type: 'gcj02',
+      success(res) {
+        const {
+          latitude,
+          longitude
+        } = res
+        qqmapsdk.reverseGeocoder({
+          location: {
+            latitude,
+            longitude
+          },
+          success(r) {
+            const {
+              address,
+              formatted_addresses
+            } = r.result
+            console.log(r.result)
+            context.setData({
+              adInfo: {
+                address: address,
+                name: formatted_addresses.recommend,
+                latitude,
+                longitude
+              }
+            })
+          },
+          fail: function (error) {
+            console.error(error)
+          },
+        })
       }
     })
   },
-  getLocation(){
-    const context = this
-    qqmapsdk.reverseGeocoder({
-      success(r){
-        const { address } = r.result
-        context.setData({
-          address
-        })
+  toChooseLocation() {
+    wx.chooseAddress({
+      success: (result) => {
+        console.log(result)
       },
-      fail: function(error) {
-        console.error(error);
-      },
+      fail: (err) => {
+        console.log(err)
+      }
     })
+    // const context = this
+    // wx.chooseLocation({
+    //   success: (res) => {
+    //     context.setData({
+    //       adInfo: {
+    //         name: res.name,
+    //         address: res.address,
+    //         latitude: res.latitude,
+    //         longitude: res.longitude
+    //       }
+    //     })
+    //   },
+    //   fail: (err) => {
+    //     console.log('---', err)
+    //   }
+    // })
   },
   onLoad() {
     this.init()
